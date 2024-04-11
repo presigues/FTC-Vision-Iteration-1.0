0f8a8302dafd61e85d325ea9c0efa4799a8a2d29
presigues <132411345+presigues@users.noreply.github.com>
2024-04-11 15:50:55 -0400

    Add files via upload

---
+++ Vision.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@TeleOp
+@Disabled
+public class Vision extends LinearOpMode {
+    private static final boolean USE_WEBCAM = true;
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+
+    // OP INIT -----------------------------------------------------------------------
+
+    @Override
+    public void runOpMode() {
+
+        initAprilTag();
+
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {  // op mode started
+            while (opModeIsActive()) {
+
+                telemetryAprilTag();
+
+                telemetry.update();
+
+                if (gamepad1.dpad_down) {
+                    visionPortal.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    visionPortal.resumeStreaming();
+                }
+
+                sleep(20);
+            }
+        }
+
+        visionPortal.close();
+
+    }
+    private void initAprilTag() {
+
+        // APRIL TAG SETTINGS --------------------------------------------------------
+
+        aprilTag = new AprilTagProcessor.Builder()
+
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagOutline(true)
+
+
+                .build();
+
+        aprilTag.setDecimation(3);
+
+
+        // VISION SETTINGS -----------------------------------------------------------
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        builder.setCameraResolution(new Size(640, 480));
+
+        builder.enableLiveView(true);
+
+        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        builder.setAutoStopLiveView(false);
+
+        builder.addProcessor(aprilTag);
+
+        visionPortal = builder.build();
+
+        visionPortal.setProcessorEnabled(aprilTag, true);
+
+    }
+
+
+    // APRIL TAG TELEMETRY ----------------------------------------------------------
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        telemetry.update();
+
+
+
+
+
+
+
+    }   // end method telemetryAprilTag()
+
+}   // end class
+
+
